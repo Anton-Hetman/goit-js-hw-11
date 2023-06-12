007b7bf8a8a8a06b406cf418af17ecce74357542
Anton Hetman <00kyp1d0n41k00@gmail.com>
2023-06-12 13:39:38 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { onFormSubmit, onLoadMoreInfo } from './js/formSubmit';
+import { refs } from './js/refs';
+
+refs.btnLoadMore.style.display = 'none';
+refs.form.addEventListener('submit', onFormSubmit);
+refs.btnLoadMore.addEventListener('click', onLoadMoreInfo);
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  form: document.querySelector('.search-form'),
+  listInfo: document.querySelector('.gallery'),
+  btnLoadMore: document.querySelector('.btnLoadMore'),
+};
